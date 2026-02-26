f93918476e9e1625c41a083d5369ebbfc479db45
callum withington <callumwithington2006@gmail.com>
2026-02-26 14:47:14 +0000

    storgae

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,21 @@
-import { create } from 'zustand'
-
-const useStore = create((set) => ({
-  tasks: [
-    { id: 1, title: "Research Zustand", column: "todo" },
-    { id: 2, title: "Build the demo", column: "doing" },
-    { id: 3, title: "Make slides", column: "done" },
-  ],
-  addTask: (title) => set((state) => ({
-    tasks: [...state.tasks, { id: Date.now(), title, column: "todo" }]
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+const useStore = create(
+  persist((set) => ({
+    tasks: [
+      { id: 1, title: "Research Zustand", column: "todo" },
+      { id: 2, title: "Build the demo", column: "doing" },
+      { id: 3, title: "Make slides", column: "done" },
+    ],
+    addTask: (title) =>
+      set((state) => ({
+        tasks: [...state.tasks, { id: Date.now(), title, column: "todo" }],
+      })),
+    moveTask: (id, column) =>
+      set((state) => ({
+        tasks: state.tasks.map((t) => (t.id === id ? { ...t, column } : t)),
+      })),
   })),
-  moveTask: (id, column) => set((state) => ({
-    tasks: state.tasks.map(t => t.id === id ? { ...t, column } : t)
-  })),
-}))
+);
 
-export default useStore
+export default useStore;
