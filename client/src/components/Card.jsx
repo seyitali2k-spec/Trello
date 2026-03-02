3213224d0418f57da7bcdfe9d05f344ac2a57618
Hat <thokenm3@gmail.com>
2026-03-02 10:51:43 +0000

    Fix draggable task card styling and structure

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,25 +3,22 @@ import { useDraggable } from "@dnd-kit/core"
 
 function Card({ task }) {
   const moveTask = useStore((state) => state.moveTask)
-  const { attributes, listeners, setNodeRef, transform } =
-  useDraggable({
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: task.id,
   })
 
   const style = {
     transform: transform
-    ? 'translate(${tranform.x}px, ${transform.y}px)'
-    : undefined,
+      ? `translate(${transform.x}px, ${transform.y}px)`
+      : undefined,
     cursor: "grab",
   }
 
   return (
     <div ref={setNodeRef} style={style} className="card">
-    <p {...listeners} {...attributes} style={{ cursor: "grab" }}>
+      <p {...listeners} {...attributes} style={{ cursor: "grab" }}>
         {task.title}
       </p>
-
-      <p>{task.title}</p>
       <div className="card-buttons">
         {task.column !== "todo" && (
           <button onClick={() => moveTask(task.id, "todo")}>To Do</button>
