e13ecb8ebe3b4e5cb9e988106dda5132a6e9f374
Hat <thokenm3@gmail.com>
2026-03-02 10:44:01 +0000

    Refactor Board component layout and remove AddTask

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,31 +1,24 @@
 import Column from './Column'
-import AddTask from './AddTask'
 import { DndContext } from "@dnd-kit/core"
 import useStore from '../store'
 
 function Board() {
   const moveTask = useStore((state) => state.moveTask)
-
   const handleDragEnd = (event) => {
     const { active, over } = event
     if (!over) return
     moveTask(active.id, over.id)
-    console.log(event)
   }
-
   return (
     <DndContext onDragEnd={handleDragEnd}>
-    <div className="board">
-      <h1> Task Board</h1>
-      <AddTask />
-      <div className="columns">
-        <Column title="To Do" columnId="todo" />
-        <Column title="Doing" columnId="doing" />
-        <Column title="Done" columnId="done" />
+      <div className="board">
+        <div className="columns">
+          <Column title="To Do" columnId="todo" />
+          <Column title="Doing" columnId="doing" />
+          <Column title="Done" columnId="done" />
+        </div>
       </div>
-    </div>
     </DndContext>
   )
 }
-
 export default Board
